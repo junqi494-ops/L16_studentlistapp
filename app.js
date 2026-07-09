ac6c4fa66379cfde45e2ad3d5eb35e5b1562ee49
Jayden <junqi494@gmail.com>
2026-07-09 13:08:51 +0800

    Lesson 18

--- app.js
+++ app.js
@@ -97,6 +97,51 @@ app.post('/addStudent', (req, res) => {
         res.redirect('/');
     });
 });
+app.get('/editStudent/:id', (req, res) => {
+    const studentid = req.params.id;
+    const sql = 'SELECT * FROM student WHERE studentid = ?';
+
+    connection.query(sql, [studentid], (error, results) => {
+        if (error) {
+            console.error('Database query error:', error.message);
+            return res.send('Error retrieving student by ID');
+        }
+
+        if (results.length > 0) {
+            res.render('editStudent', { student: results[0] });
+        } else {
+            res.send('Student not found');
+        }
+    });
+});
+app.post('/editStudent/:id', (req, res) => {
+    const studentid = req.params.id;
+    const { name, dob, contact, image } = req.body;
+
+    const sql = 'UPDATE student SET name = ?, dob = ?, contact = ?, image = ? WHERE studentid = ?';
+
+    connection.query(sql, [name, dob, contact, image, studentid], (error, results) => {
+        if (error) {
+            console.error("Error updating student:", error);
+            res.send('Error updating student');
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+app.get('/deleteStudent/:id', (req, res) => {
+    const studentid = req.params.id;
+    const sql = 'DELETE FROM student WHERE studentid = ?';
+
+    connection.query(sql, [studentid], (error, results) => {
+        if (error) {
+            console.error("Error deleting student:", error);
+            res.send('Error deleting student');
+        } else {
+            res.redirect('/');
+        }
+    });
+});
 
 const PORT = process.env.PORT || 3000;
 
